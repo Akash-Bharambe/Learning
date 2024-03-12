e6276bcc52d60a678aec1067624eb53ef612beb0
Akash-Bharambe <aakashbharambe100@gmail.com>
2024-03-12 16:05:25 +0530

    added

--- Cpp/Practise/matrixMulti/matrixFunction.cpp
+++ Cpp/Practise/matrixMulti/matrixFunction.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
+#include "matrixHead.h"
 using namespace std;
-
-#define SIZE 3
 
 void acceptMatrix(int a[][SIZE])
 {
@@ -24,32 +23,19 @@ void matrixMultiply(int a[][SIZE], int b[][SIZE], int r[][SIZE])
             r[x][y] = 0;
             for (int z = 0; z < SIZE; z++)
             {
-                r[x][y] += a[x][z] * b[z][y];  
+                r[x][y] += a[x][z] * b[z][y];
             }
         }
     }
 }
 
-void displayMatrix(int a[][SIZE]){
+void displayMatrix(int a[][SIZE])
+{
     for (int x = 0; x < SIZE; x++)
     {
         for (int y = 0; y < SIZE; y++)
         {
-            cout << "i" << x + 1 << " j" << y + 1 << ": "<<a[x][y]<<endl;
+            cout << "i" << x + 1 << " j" << y + 1 << ": " << a[x][y] << endl;
         }
     }
-}
-int main()
-{
-    int a[SIZE][SIZE], b[SIZE][SIZE], r[SIZE][SIZE];
-    cout << "Enter a";
-    acceptMatrix(a);
-    cout << "Enter b";
-    acceptMatrix(b);
-
-    matrixMultiply(a, b, r);
-
-    displayMatrix(r);
-
-    return 0;
 }
